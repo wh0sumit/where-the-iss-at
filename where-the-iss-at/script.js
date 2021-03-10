c7358ca03599df004b397508e69d0f880896402f
SUMIT SINGH <76095902+WH0SUMIT@users.noreply.github.com>
2021-03-10 17:15:58 +0530

    Update script.js

--- where-the-iss-at/script.js
+++ where-the-iss-at/script.js
@@ -3,18 +3,22 @@ $(document).ready(() => {
     async function getIss() {
         const response = await fetch(`https://api.wheretheiss.at/v1/satellites/25544`);
         const data = await response.json();
-        const {
-            longitude,
-            latitude,
-            timestamp,
-            altitude,
-            velocity
-        } = data;
-        $('.latitude').html(latitude);
-        $('.longitude').html(longitude);
-        $('.time').html(timestamp);
-        $('.altitude').html(altitude);
-        $('.velocity').html(velocity);
+        const issData = {
+            id: data.id,
+            name: data.name.toUpperCase(),
+            time: data.timestamp,
+            latitude: data.latitude,
+            longitude: data.longitude,
+            velocity: data.velocity,
+            altitude: data.altitude,
+        }
+        $('.id').html(issData.id);
+        $('.name').html(issData.name);
+        $('.latitude').html(issData.latitude);
+        $('.longitude').html(issData.longitude);
+        $('.time').html(issData.time);
+        $('.altitude').html((issData.altitude * 0.62137).toFixed(2) + " miles");
+        $('.velocity').html((issData.velocity * 0.62137).toFixed(2) + " mph");
         $('.card-footer').html(new Date());
     }
     getIss();
